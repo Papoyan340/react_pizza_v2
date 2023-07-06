955e0740d259a1e4a09be72a382196050fc61338
Artur Papoyan <papoyan.340@gmail.com>
2023-07-06 15:27:07 +0400

    added search and Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -18,32 +18,15 @@ function App() {
    // const pathname = window.location.pathname
    // ............... linki hascen imanalu hamar end ....................
 
-   const count = useSelector((state) => state.counter.value);
-   const dispatch = useDispatch();
-   console.log(count);
+  
    const [searcValue, setSearchValue] = React.useState('');
 
-   const obj = {
-      id: 5,
-      name: 'alla',
-      age: 32
-   }
+   
 
    return (
       <div className="wrapper">
-         <div>
-            <div>
-               <button onClick={()=>dispatch(addUser(obj))}>add</button>
-               {
-                  count.map((el)=> <div key={el.id}>
-                     <h1>{el.name}</h1>
-                     <p>{el.age}</p>
-                  </div>)
-               }
-            </div>
-         </div>
-
-         {/* <SearchContext.Provider value={{ searcValue, setSearchValue }}>
+   
+         <SearchContext.Provider value={{ searcValue, setSearchValue }}>
             <Header />
             <div className="content">
                <div className="container">
@@ -57,7 +40,7 @@ function App() {
                   </Routes>
                </div>
             </div>
-         </SearchContext.Provider> */}
+         </SearchContext.Provider>
       </div>
    );
 }
